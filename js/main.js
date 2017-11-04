7867329bd8ead9881c824177c9110125b058e62d
MizukiFurusawa <fmzk0326@gmail.com>
2017-11-05 00:17:43 +0900

    #09

---
+++ js/main.js
@@ -0,0 +1,9 @@
+(function(){
+  'use strict';
+  var higher = document.getElementById('higher');
+  var lower = document.getElementById('lower');
+  var dealerCard = document.getElementById('dealer_card');
+  var playerCard = document.getElementById('dealer_card');
+  var wrapper = document.getElementById('wrapper');
+  var result = document.getElementById('result');
+})();
