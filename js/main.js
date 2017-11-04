7095ae0b96bece2d96e52955da5b52a60ada3f8b
MizukiFurusawa <fmzk0326@gmail.com>
2017-11-05 00:43:01 +0900

    #10

--- js/main.js
+++ js/main.js
@@ -3,7 +3,22 @@
   var higher = document.getElementById('higher');
   var lower = document.getElementById('lower');
   var dealerCard = document.getElementById('dealer_card');
-  var playerCard = document.getElementById('dealer_card');
+  var playerCard = document.getElementById('player_card');
   var wrapper = document.getElementById('wrapper');
   var result = document.getElementById('result');
+  var dealerValue;
+  var playerValue;
+
+  function getRandom() {
+    return Math.floor(Math.random() * 13 + 1);
+  }
+
+  function init() {
+    dealerValue = getRandom();
+    dealerCard.textContent = dealerValue;
+    playerValue = getRandom();
+    playerCard.textContent = playerValue;
+  }
+
+  init();
 })();
